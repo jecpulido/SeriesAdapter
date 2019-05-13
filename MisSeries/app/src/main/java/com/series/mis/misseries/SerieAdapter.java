ca356a3fde5db18e8c3220c662ee4c3e99dc27aa
jecpulido <jorgepulido95@hotmail.com>
2019-05-13 06:25:49 -0500

    Adapters

--- MisSeries/app/src/main/java/com/series/mis/misseries/SerieAdapter.java
+++ MisSeries/app/src/main/java/com/series/mis/misseries/SerieAdapter.java
@@ -1,5 +1,42 @@
 package com.series.mis.misseries;
 
-public class SerieAdapter  {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class SerieAdapter extends RecyclerView.Adapter<SerieAdapter.ViewHolderSerie> {
+
+    public ArrayList<Serie> lstRequest;
+    public Context vContext;
+
+    public SerieAdapter(ArrayList<Serie> lstRequest, Context vContext) {
+        this.lstRequest = lstRequest;
+        this.vContext = vContext;
+    }
+
+    @Override
+    public SerieAdapter.ViewHolderSerie onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SerieAdapter.ViewHolderSerie viewHolderSerie, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolderSerie extends RecyclerView.ViewHolder {
+        public ViewHolderSerie(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
 
--- MisSeries/app/src/main/java/com/series/mis/misseries/SerieDetailAdapter.java
+++ MisSeries/app/src/main/java/com/series/mis/misseries/SerieDetailAdapter.java
@@ -1,4 +1,44 @@
 package com.series.mis.misseries;
 
-public class SerieDetailAdapter {
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class SerieDetailAdapter extends RecyclerView.Adapter<SerieDetailAdapter.ViewHolderDetail>{
+
+    public ArrayList<DetailSerie> lstRequest;
+    public Context vContext;
+
+    public SerieDetailAdapter(ArrayList<DetailSerie> lstRequest, Context vContext) {
+        this.lstRequest = lstRequest;
+        this.vContext = vContext;
+    }
+
+
+    @Override
+    public ViewHolderDetail onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDetail viewHolderDetail, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolderDetail extends RecyclerView.ViewHolder {
+
+        public ViewHolderDetail(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
